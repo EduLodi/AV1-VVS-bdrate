6867363426dcbb5623cff391e1f5cbc0e776675f
Eduardo Lodi <91179686+EduLodi@users.noreply.github.com>
2022-06-20 17:13:22 -0300

    Update encode_script.py

--- encode_script.py
+++ encode_script.py
@@ -1,10 +1,14 @@
 import os
+import csv
+from csv import writer
 
 YUV_PATH = "./videos"
 VTM_PATH = "./VTM17.0/VVCSoftware_VTM-VTM-17.0/bin/EncoderAppStatic"
 VTM_CFG_PATH = "./VTM17.0/VVCSoftware_VTM-VTM-17.0/cfg"
-SAVES_PATH = "saved_rates"
+VTM_SAVES_PATH = "VTM_saved_rates.csv"
+AV1_SAVES_PATH = "/home/edulodi/documents/AV1_saved_rates.csv"
 BR_STRING = "\tTotal Frames |  Bitrate      Y-PSNR   U-PSNR   V-PSNR   YUV-PSNR \n"
+FRAMES_AMNT = 2
 
 videos = os.listdir(YUV_PATH)
 #qps = [22, 27, 32, 37]
@@ -17,22 +21,44 @@ def parse_vtm_output(pt):
         psnr_string = out_string[results_index].split()[6]
     return (float(bitrate_string), float(psnr_string))
 
+def append_to_csv(csv_path, encoder, qp_value, video_name, psnr_value, br_value):
+    with open(csv_path, 'a') as (csv_list):
+        writer_object = writer(csv_list)
+        writer_object.writerow([encoder, qp_value, video_name, psnr_value, br_value])
+
+def csv_to_lists(csv_path): #returns 2 lists, the first one with the bitrates, and the second with the psnr
+    bitters = []
+    psnrers = []
+    with open(csv_path, 'r') as csv_input:
+        csv_reader = csv.reader(csv_input, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count > 0:
+                bitters.append(row[4])
+                psnrers.append(row[3])
+            line_count += 1
+    return bitters, psnrers
 
 for video in videos:
+    
+    base_yuv_name = video.split("_")[0]
+    yuv_full_path = "%s/%s" % (YUV_PATH, video)
+    
     for qp in qps:
-        base_yuv_name = video.split("_")[0]
-        yuv_full_path = "%s/%s" % (YUV_PATH, video)
+        
+        ##############################RODANDO VTM############################################
         output_path = "saida_%s_qp%d.txt" % (base_yuv_name, qp)
         vtm_parameters = "-c %s/encoder_randomaccess_vtm.cfg " % (VTM_CFG_PATH)
         vtm_parameters += "-c %s/per-sequence/%s.cfg " % (VTM_CFG_PATH, base_yuv_name)
         vtm_parameters += "--InputFile=%s " %(yuv_full_path)
-        vtm_parameters += "--QP=%d -f 2" % (qp)
+        vtm_parameters += "--QP=%d -f %i" % (qp,FRAMES_AMNT)
         vtm_parameters += "--BitstreamFile=%s_qp%d.bin " % (base_yuv_name, qp)
         encode_cmd = "%s %s > %s" % (VTM_PATH, vtm_parameters, output_path)
        # print(encode_cmd)
        # os.system(encode_cmd)
-        br, psnr = parse_vtm_output(output_path)
-        print (br, psnr)
-        with open(SAVES_PATH, 'w') as saves_list:
-           saves_list.write("%s,%s.PSNR: %f\n" % (str(qp), base_yuv_name, psnr))
-           saves_list.write("%s,%s.BITRATE: %f\n" % (str(qp), base_yuv_name, br))
+        br_vtm, psnr_vtm = parse_vtm_output(output_path)
+        print (br_vtm, psnr_vtm)
+        append_to_csv(VTM_SAVES_PATH, 'VTM', qp, base_yuv_name, psnr_vtm, br_vtm)
+        ########################################################################################
+
+        
