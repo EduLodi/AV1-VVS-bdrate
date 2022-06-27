f0af864434a936425140d3c580fb2443198ea080
Eduardo Lodi <91179686+EduLodi@users.noreply.github.com>
2022-06-27 17:33:55 -0300

    Update encode_script.py

--- encode_script.py
+++ encode_script.py
@@ -5,7 +5,7 @@ import bjoontegaard_metric
 
 ###############################GENERAL CONSTANTS##################################
 YUV_PATH = "/home/edulodi/documents/videos"
-FRAMES_AMNT = 2
+FRAMES_AMNT = 5
 ##################################################################################
 
 ##################################LIBAOM AV1 CONSTANTS############################
@@ -19,9 +19,8 @@ SVT_AV1_PATH = "/home/edulodi/SVT_AV1/SVT-AV1/Bin/Release/SvtAv1EncApp"
 ##################################################################################
 
 videos = os.listdir(YUV_PATH)
-#qps = [22, 27, 32, 37]
-qps = [22]
-
+qps = [22, 27, 32, 37]
+#qps = [22]
 
 def parse_svt_output(pt1,pt2):
     BR_STRING = 'Total Frames	Average QP  	Y-PSNR   	U-PSNR   	V-PSNR		| 	Y-PSNR   	U-PSNR   	V-PSNR   	|	Y-SSIM   	U-SSIM   	V-SSIM   	|	Bitrate\n'
@@ -53,7 +52,7 @@ def append_to_csv(csv_path, encoder, qp_value, video_name, psnr_value, br_value,
         writer_object = writer(csv_list)
         writer_object.writerow([encoder, qp_value, video_name, psnr_value, br_value,time_ms])
 
-def csv_to_lists(csv_path): #returns 2 lists, the first one with the bitrates, and the second with the psnr
+def csv_to_lists(csv_path, list_qp): #returns 2 lists, the first one with the bitrates, and the second with the psnr
     bitters = []
     psnrers = []
     with open(csv_path, 'r') as csv_input:
@@ -61,8 +60,11 @@ def csv_to_lists(csv_path): #returns 2 lists, the first one with the bitrates, a
         line_count = 0
         for row in csv_reader:
             if line_count > 0:
-                bitters.append(row[4])
-                psnrers.append(row[3])
+                if row != []:
+                    if int(row[1]) == list_qp:
+                        print(row[1])
+                        bitters.append(row[4])
+                        psnrers.append(row[3])
             line_count += 1
     return bitters, psnrers
 
@@ -74,27 +76,39 @@ for video in videos:
     base_yuv_name = video.split("_")[0]
 
     for qp in qps:
-
+        continue
 
         ##############################RODANDO SVT_AV1###########################################
-        output_path_svt = "/home/edulodi/SVT_AV1/output_files/svt_saida_%s_qp%d.txt" % (base_yuv_name, qp)
-        output_path_svt_time = "/home/edulodi/SVT_AV1/output_files/svt_time_saida_%s_qp%d.txt" % (base_yuv_name, qp)
-        svt_parameters = " -i %s -n %i" % (yuv_full_path, FRAMES_AMNT)
-        svt_parameters += " --enable-stat-report 1 --stat-file %s --lp 1" % (output_path_svt)
-        svt_parameters += " --qp %d --max-qp %d --min-qp %d --rc 0" % (qp,qp,qp)
-        encode_cmd_svt = "%s %s &> %s" % (SVT_AV1_PATH, svt_parameters, output_path_svt_time)
-        print(encode_cmd_svt)
-        os.system(encode_cmd_svt)
-        br_svt, psnr_svt, timems_svt = parse_svt_output(output_path_svt, output_path_svt_time)
-        append_to_csv(SVT_AV1_SAVES_PATH, 'SVT', qp, base_yuv_name, psnr_svt, br_svt, timems_svt)
+        #output_path_svt = "/home/edulodi/SVT_AV1/output_files/svt_saida_%s_qp%s.txt" % (base_yuv_name, qp)
+        #output_path_svt_time = "/home/edulodi/SVT_AV1/output_files/svt_time_saida_%s_qp%s.txt" % (base_yuv_name, qp)
+        #svt_parameters = " -i %s -n %i" % (yuv_full_path, FRAMES_AMNT)
+        #svt_parameters += " --enable-stat-report 1 --stat-file %s --lp 1" % (output_path_svt)
+        #svt_parameters += " --qp %s --max-qp %s --min-qp %s --rc 0" % (qp,qp,qp)
+        #encode_cmd_svt = "%s %s 2> %s" % (SVT_AV1_PATH, svt_parameters, output_path_svt_time)
+        #print(encode_cmd_svt)
+        #os.system(encode_cmd_svt)
+        #br_svt, psnr_svt, timems_svt = parse_svt_output(output_path_svt, output_path_svt_time)
+        #append_to_csv(SVT_AV1_SAVES_PATH, 'SVT', qp, base_yuv_name, psnr_svt, br_svt, timems_svt)
         ########################################################################################
 
         ##############################RODANDO LIBAOM_AV1############################################
-        output_path_libaom = "/home/edulodi/documents/output_aom/aom_saida_%s_qp%s.txt" % (base_yuv_name,qp)
-        aom_parameters = " limit=%s --tune=psnr --psnr --cq-level=%d" % (FRAMES_AMNT, qp)
-        aom_parameters = " -o /home/edulodi/documents/output_aom/aom_vid_saida_%s_qp%s.webm %s" % (base_yuv_name,qp, yuv_full_path)
-        encode_cmd_aom = "%s %s &> %s" % (LIBAOM_AV1_PATH, aom_parameters, output_path_libaom)
-        os.system(encode_cmd_aom)
-        br_aom, psnr_aom, timems_aom = parse_aom_output(output_path_libaom)
-        append_to_csv(LIBAOM_AV1_SAVES_PATH, 'AOM', qp, base_yuv_name, psnr_aom, br_aom, timems_aom)
+        #output_path_libaom = "/home/edulodi/documents/output_aom/aom_saida_%s_qp%s.txt" % (base_yuv_name,qp)
+        #aom_parameters = " --limit=%i --psnr --cq-level=%s --frame-parallel=0 --cpu-used=1" % (FRAMES_AMNT, qp)
+        #aom_parameters += " -o /home/edulodi/documents/output_aom/aom_vid_saida_%s_qp%s.webm %s" % (base_yuv_name,qp, yuv_full_path)
+        #encode_cmd_aom = "%s %s > %s 2>&1" % (LIBAOM_AV1_PATH, aom_parameters, output_path_libaom)
+        #print(encode_cmd_aom)
+        #os.system(encode_cmd_aom)
+        #br_aom, psnr_aom, timems_aom = parse_aom_output(output_path_libaom)
+        #append_to_csv(LIBAOM_AV1_SAVES_PATH, 'AOM', qp, base_yuv_name, psnr_aom, br_aom, timems_aom)
         ########################################################################################
+
+br_22_svt , psnr_22_svt = csv_to_lists(SVT_AV1_SAVES_PATH,22)
+br_27_svt , psnr_27_svt = csv_to_lists(SVT_AV1_SAVES_PATH,27)
+br_32_svt , psnr_32_svt = csv_to_lists(SVT_AV1_SAVES_PATH,32)
+br_37_svt , psnr_37_svt = csv_to_lists(SVT_AV1_SAVES_PATH,37)
+br_22_aom , psnr_22_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,22)
+br_27_aom , psnr_27_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,27)
+br_32_aom , psnr_32_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,32)
+br_37_aom , psnr_37_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,37)
+
+print(br_22_svt)
