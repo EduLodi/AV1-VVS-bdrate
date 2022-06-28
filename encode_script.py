80bd22e75d7f646de5086431270c50b700779268
Eduardo Lodi <91179686+EduLodi@users.noreply.github.com>
2022-06-28 15:20:42 -0300

    Update encode_script.py

--- encode_script.py
+++ encode_script.py
@@ -5,24 +5,24 @@ import bjoontegaard_metric
 import matplotlib.pyplot as plt
 
 ###############################GENERAL CONSTANTS##################################
-YUV_PATH = "/home/edulodi/documents/videos"
-FRAMES_AMNT = 5
+YUV_PATH = #path to the folder containing the raw videos
+FRAMES_AMNT = #number of frames to encode
 ##################################################################################
 
 ##################################LIBAOM AV1 CONSTANTS############################
-LIBAOM_AV1_SAVES_PATH = "/home/edulodi/documents/LIBAOM_AV1_saved_rates.csv"
-LIBAOM_AV1_PATH = "/home/edulodi/documents/aom/bin/aomenc"
+LIBAOM_AV1_SAVES_PATH = "csv_data_av1_encoders/LIBAOM_AV1_saved_rates.csv"
+LIBAOM_AV1_PATH = "/home/edulodi/documents/aom/bin/aomenc" # aomenc path
 ##################################################################################
 
 #######################SVT AV1 CONSTANTS##########################################
-SVT_AV1_SAVES_PATH = "/home/edulodi/documents/SVT_AV1_saved_rates.csv"
-SVT_AV1_PATH = "/home/edulodi/SVT_AV1/SVT-AV1/Bin/Release/SvtAv1EncApp"
+SVT_AV1_SAVES_PATH = "csv_data_av1_encoders/SVT_AV1_saved_rates.csv" #
+SVT_AV1_PATH = "/home/edulodi/SVT_AV1/SVT-AV1/Bin/Release/SvtAv1EncApp" #SvtAv1EncApp path
 ##################################################################################
 
 videos = os.listdir(YUV_PATH)
-qps = [22, 27, 32, 37]
-#qps = [22]
+qps = [22, 27, 32, 37] #list of qps for encoding
 
+#function to parse the output of svt encoding, returns 3 floats: bitrate, psnr and time in ms
 def parse_svt_output(pt1,pt2):
     BR_STRING = 'Total Frames	Average QP  	Y-PSNR   	U-PSNR   	V-PSNR		| 	Y-PSNR   	U-PSNR   	V-PSNR   	|	Y-SSIM   	U-SSIM   	V-SSIM   	|	Bitrate\n'
     with open(pt1, 'rt') as output_text:
@@ -38,6 +38,7 @@ def parse_svt_output(pt1,pt2):
         timems_string = strtime.split()[3]
     return float(bitrate_string)*1024, float(psnr_string) , float(timems_string)
 
+#function to parse the output of svt encoding, returns 3 floats: bitrate, psnr and time in ms
 def parse_aom_output(st):
     with open(st, 'rt') as output_text:
         out_string = output_text.readlines()
@@ -48,11 +49,13 @@ def parse_aom_output(st):
                 ms_stringaom = line.split()[11]
     return float(bitrateaom_string), float(psnr_stringaom), float(ms_stringaom)
 
+#function to append the values to a pre created csv list
 def append_to_csv(csv_path, encoder, qp_value, video_name, psnr_value, br_value, time_ms):
     with open(csv_path, 'a') as (csv_list):
         writer_object = writer(csv_list)
         writer_object.writerow([encoder, qp_value, video_name, psnr_value, br_value,time_ms])
 
+#function to get the values from the csv list and separate them into lists according to the video's name
 def csv_to_lists(csv_path, video_name): #returns 2 lists, the first one with the bitrates, and the second with the psnr
     bitters = []
     psnrers = []
@@ -78,55 +81,68 @@ for video in videos:
     base_yuv_name = video.split("_")[0]
 
     for qp in qps:
-
-        ##############################RODANDO SVT_AV1###########################################
-        #output_path_svt = "/home/edulodi/SVT_AV1/output_svt/svt_saida_%s_qp%s.txt" % (base_yuv_name, qp)
-        #output_path_svt_time = "/home/edulodi/SVT_AV1/output_svt/svt_time_saida_%s_qp%s.txt" % (base_yuv_name, qp)
-        #svt_parameters = " -i %s -n %i" % (yuv_full_path, FRAMES_AMNT)
-        #svt_parameters += " --enable-stat-report 1 --stat-file %s --lp 1" % (output_path_svt)
-        #svt_parameters += " --qp %s --max-qp %s --min-qp %s --rc 0" % (qp,qp,qp)
-        #encode_cmd_svt = "%s %s 2> %s" % (SVT_AV1_PATH, svt_parameters, output_path_svt_time)
-        #print(encode_cmd_svt)
-        #os.system(encode_cmd_svt)
-        #br_svt, psnr_svt, timems_svt = parse_svt_output(output_path_svt, output_path_svt_time)
-        #append_to_csv(SVT_AV1_SAVES_PATH, 'SVT', qp, base_yuv_name, psnr_svt, br_svt, timems_svt)
+       
+        ##############################EXECUTING SVT_AV1###########################################
+        output_path_svt = "output_svt/svt_saida_%s_qp%s.txt" % (base_yuv_name, qp)
+        output_path_svt_time = "output_svt/svt_time_saida_%s_qp%s.txt" % (base_yuv_name, qp)
+        svt_parameters = " -i %s -n %i" % (yuv_full_path, FRAMES_AMNT)
+        svt_parameters += " --enable-stat-report 1 --stat-file %s --lp 1" % (output_path_svt)
+        svt_parameters += " --qp %s --max-qp %s --min-qp %s --rc 0" % (qp,qp,qp)
+        encode_cmd_svt = "%s %s 2> %s" % (SVT_AV1_PATH, svt_parameters, output_path_svt_time)
+        print(encode_cmd_svt)
+        os.system(encode_cmd_svt)
+        br_svt, psnr_svt, timems_svt = parse_svt_output(output_path_svt, output_path_svt_time)
+        append_to_csv(SVT_AV1_SAVES_PATH, 'SVT', qp, base_yuv_name, psnr_svt, br_svt, timems_svt)
         ########################################################################################
 
-        ##############################RODANDO LIBAOM_AV1############################################
-        output_path_libaom = "/home/edulodi/documents/output_aom/aom_saida_%s_qp%s.txt" % (base_yuv_name,qp)
-        aom_parameters = " --limit=%i --verbose --cq-level=%s --cpu-used=1" % (FRAMES_AMNT, qp)
-        aom_parameters += " -o /home/edulodi/documents/output_aom/aom_vid_saida_%s_qp%s.webm %s" % (base_yuv_name,qp, yuv_full_path)
+        #############################EXECUTING LIBAOM_AV1############################################
+        output_path_libaom = "output_aom/aom_saida_%s_qp%s.txt" % (base_yuv_name,qp)
+        aom_parameters = " --limit=%i --verbose --psnr --cq-level=%s --end-usage=q --cpu-used=1" % (FRAMES_AMNT, qp)
+        aom_parameters += " -o output_aom/aom_vid_saida_%s_qp%s.webm %s" % (base_yuv_name,qp, yuv_full_path)
         encode_cmd_aom = "%s %s > %s 2>&1 " % (LIBAOM_AV1_PATH, aom_parameters, output_path_libaom)
         print(encode_cmd_aom)
         os.system(encode_cmd_aom)
         br_aom, psnr_aom, timems_aom = parse_aom_output(output_path_libaom)
         append_to_csv(LIBAOM_AV1_SAVES_PATH, 'AOM', qp, base_yuv_name, psnr_aom, br_aom, timems_aom)
-        ########################################################################################
+        #######################################################################################
 
-#br_foreman_svt , psnr_foreman_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"foreman")
-#br_akiyo_svt , psnr_akiyo_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"akiyo")
-#br_flower_svt , psnr_flower_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"flower")
-#br_carphone_svt , psnr_carphone_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"carphone")
-#br_foreman_aom , psnr_foreman_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"foreman")
-#br_akiyo_aom , psnr_akiyo_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"akiyo")
-#br_flower_aom , psnr_flower_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"flower")
-#br_carphone_aom , psnr_carphone_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"carphone")
-#
-#print(br_foreman_svt,"\n",psnr_foreman_svt,"\n",br_foreman_aom,"\n",psnr_foreman_aom)
-#
-#bd_rate_foreman = bjoontegaard_metric.BD_PSNR(br_foreman_svt,psnr_foreman_svt,br_foreman_aom,psnr_foreman_aom)
-#bd_rate_akiyo = bjoontegaard_metric.BD_PSNR(br_akiyo_svt,psnr_akiyo_svt,br_akiyo_aom,psnr_akiyo_aom)
-#bd_rate_flower = bjoontegaard_metric.BD_PSNR(br_flower_svt , psnr_flower_svt,br_flower_aom , psnr_flower_aom)
-#bd_rate_carphone = bjoontegaard_metric.BD_PSNR(br_carphone_svt , psnr_carphone_svt,br_carphone_aom , psnr_carphone_aom)
-#
-#print(bd_rate_foreman, bd_rate_akiyo, bd_rate_flower, bd_rate_carphone)
+##################CREATING THE LISTS###########################################
+br_foreman_svt , psnr_foreman_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"foreman")
+br_akiyo_svt , psnr_akiyo_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"akiyo")
+br_flower_svt , psnr_flower_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"flower")
+br_carphone_svt , psnr_carphone_svt = csv_to_lists(SVT_AV1_SAVES_PATH,"carphone")
+br_foreman_aom , psnr_foreman_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"foreman")
+br_akiyo_aom , psnr_akiyo_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"akiyo")
+br_flower_aom , psnr_flower_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"flower")
+br_carphone_aom , psnr_carphone_aom = csv_to_lists(LIBAOM_AV1_SAVES_PATH,"carphone")
 
-#########################plotting###############################
-#x = ["foreman","akiyo","flower","carphone"]
-#y = [bd_rate_foreman, bd_rate_akiyo, bd_rate_flower, bd_rate_carphone]
-#
-#plt.plot(x,y)
-#plt.xlabel("QPs")
-#plt.ylabel("BD")
-#plt.title("BD_RATE AV1 - AOM x SVT")
-#plt.show()
+###############################CALCULATING BD-RATE###########################################################
+bd_rate_foreman = bjoontegaard_metric.BD_RATE(br_foreman_aom,psnr_foreman_aom, br_foreman_svt,psnr_foreman_svt)
+bd_rate_akiyo = bjoontegaard_metric.BD_RATE(br_akiyo_aom,psnr_akiyo_aom,br_akiyo_svt,psnr_akiyo_svt)
+bd_rate_flower = bjoontegaard_metric.BD_RATE(br_flower_aom , psnr_flower_aom, br_flower_svt , psnr_flower_svt)
+bd_rate_carphone = bjoontegaard_metric.BD_RATE(br_carphone_aom , psnr_carphone_aom,br_carphone_svt , psnr_carphone_svt)
+
+#####################################CALCULATING BD-PSNR##########################################################
+bd_PSNR_foreman = bjoontegaard_metric.BD_PSNR(br_foreman_aom,psnr_foreman_aom, br_foreman_svt,psnr_foreman_svt)
+bd_PSNR_akiyo = bjoontegaard_metric.BD_PSNR(br_akiyo_aom,psnr_akiyo_aom,br_akiyo_svt,psnr_akiyo_svt)
+bd_PSNR_flower = bjoontegaard_metric.BD_PSNR(br_flower_aom , psnr_flower_aom, br_flower_svt , psnr_flower_svt)
+bd_PSNR_carphone = bjoontegaard_metric.BD_PSNR(br_carphone_aom , psnr_carphone_aom,br_carphone_svt , psnr_carphone_svt)
+
+#########################PLOTTING###############################
+x = ["foreman","akiyo","flower","carphone"]
+y = [bd_rate_foreman, bd_rate_akiyo, bd_rate_flower, bd_rate_carphone]
+
+plt.bar(x,y)
+plt.xlabel("Videos")
+plt.ylabel("BD_RATE")
+plt.title("BD_RATE AV1 - AOM x SVT")
+plt.show()
+
+x = ["foreman","akiyo","flower","carphone"]
+y = [bd_PSNR_foreman, bd_PSNR_akiyo, bd_PSNR_flower, bd_PSNR_carphone]
+
+plt.bar(x,y)
+plt.xlabel("Videos")
+plt.ylabel("BD_PSNR")
+plt.title("BD_PSNR AV1 - AOM x SVT")
+plt.show()
